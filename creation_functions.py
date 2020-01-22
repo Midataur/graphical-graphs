51024bee42382d6d1b2c904ca9b7694edce50a9c
Midataur <maxpetschack2@gmail.com>
2020-01-23 09:48:21 +1100

    Fixed how the nodes numbering works
    Previously if you deleted a node using undo and then remade it using create_node it would skip and id. Now when a node is created it correctly uses the lowest free id.

--- creation_functions.py
+++ creation_functions.py
@@ -1,6 +1,8 @@
 from pynode.main import *
 import pickle
 
+highest_id = 0
+
 def save(*args,**kwargs):
     data = graph.nodes()+graph.edges()
     data = [node for node in data if not node.attribute('special')]
@@ -8,11 +10,14 @@ def save(*args,**kwargs):
     print('Saved!')
 
 def undo(history,*args,**kwargs):
+    global highest_id
     if not history.empty():
         last_creation = history.get()
         #i know remove_all looks like a weird decision
         #but it's the only type agnostic way to remove something
         graph.remove_all([last_creation])
+        if type(last_creation) == Node:
+            highest_id -= 1
 
 def create_edge(selected,new_selection,*args,**kwargs):
     new_edge = Edge(selected,new_selection)
@@ -21,7 +26,9 @@ def create_edge(selected,new_selection,*args,**kwargs):
     return new_edge
 
 def create_node(history,*args,**kwargs):
-    new_node = Node()
+    global highest_id
+    new_node = Node(highest_id+1)
+    highest_id +=1
     new_node.set_attribute('special',False)
     graph.add_node(new_node)
     history.put(new_node)
