509ef910fb68c9ef6c22a147587252f5972feb30
Midataur <maxpetschack2@gmail.com>
2020-01-22 13:38:34 +1100

    Implemented an undo button
    TODO: make it so if a node is removed its id is put back into the pool

--- creation_functions.py
+++ creation_functions.py
@@ -1,19 +1,27 @@
 from pynode.main import *
 import pickle
 
-def save():
+def save(*args,**kwargs):
     data = graph.nodes()+graph.edges()
     data = [node for node in data if not node.attribute('special')]
     pickle.dump(data,open('graph.g','wb'))
     print('Saved!')
 
-def create_edge(selected,new_selection):
-    #otherwise create the edge
-    print(selected,new_selection)
-    graph.add_edge(Edge(selected,new_selection))
-    selected.set_color(Color.DARK_GREY)
+def undo(history,*args,**kwargs):
+    if not history.empty():
+        last_creation = history.get()
+        #i know remove_all looks like a weird decision
+        #but it's the only type agnostic way to remove something
+        graph.remove_all([last_creation])
 
-def create_node():
+def create_edge(selected,new_selection,*args,**kwargs):
+    new_edge = Edge(selected,new_selection)
+    graph.add_edge(new_edge)
+    selected.set_color(Color.DARK_GREY)
+    return new_edge
+
+def create_node(history,*args,**kwargs):
     new_node = Node()
     new_node.set_attribute('special',False)
     graph.add_node(new_node)
+    history.put(new_node)
--- creator.py
+++ creator.py
@@ -1,9 +1,11 @@
 from pynode.main import *
 from creation_functions import *
 import pickle
+import queue
 
 #globals
 selected = None
+history = queue.LifoQueue()
 special_nodes = {
     'save':{
         'function':save,
@@ -12,18 +14,23 @@ special_nodes = {
     'create_node':{
         'function':create_node,
         'colour':Color.YELLOW
+    },
+    'undo':{
+        'function':undo,
+        'colour':Color.RED
     }
 }
 
 def on_click(new_selection):
-    global selected,special
+    global selected,special,history
     if new_selection.id() in special_nodes.keys():
-        return special_nodes[new_selection.id()]['function']()
+        return special_nodes[new_selection.id()]['function'](history=history)
     if not selected:
         selected = new_selection
         new_selection.set_color(Color.BLUE)
     else:
-        create_edge(selected,new_selection)
+        new_edge = create_edge(selected,new_selection)
+        history.put(new_edge)
         selected = None
 
 def run():
